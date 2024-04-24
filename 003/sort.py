193fa573cd658a7b2fe51a317329dd423b685d66
Aaron Kuhn <160550472+Aaron0049@users.noreply.github.com>
2024-04-24 11:01:45 -0300

    added selection sort plus tweaks

--- 003/sort.py
+++ 003/sort.py
@@ -1,18 +1,14 @@
-#Bubble sort. Uses a temp variable for comparisons. Slow, only use for small lists unless you like waiting.
+#Bubble sort. Uses no auxiliary space (I think?). Slow, only use for small lists unless you like waiting.
 def bubble_manual_sort(list: list, *, order: str) -> int:
     for i in range(len(list)):
         for j in range(0, len(list) - i - 1):
             match order:
                 case "asc":
                     if list[j] > list[j + 1]:
-                        temp = list[j]
-                        list[j] = list[j+1]
-                        list[j+1] = temp
+                        list[j], list[j + 1] = list[j + 1], list[j]
                 case "desc":
                     if list[j] < list[j + 1]:
-                        temp = list[j]
-                        list[j] = list[j+1]
-                        list[j+1] = temp
+                        list[j], list[j + 1] = list[j + 1], list[j]
     return list
 
 #Quick sort. Left as manual_sort to function with pre written unit tests.
@@ -21,19 +17,32 @@ def manual_sort(list: list, *, order: str) -> int:
     if len(list) < 2:
         return list
     else:
+        pivot= list[0]
+        less = [i for i in list[1:] if i <= pivot]
+        greater = [i for i in list[1:] if i > pivot]
         match order:
             case "asc":
-                pivot= list[0]
-                less = [i for i in list[1:] if i <= pivot]
-                greater = [i for i in list[1:] if i > pivot]
                 return manual_sort(less, order="asc") + [pivot] + manual_sort(greater, order="asc")
             case "desc":
-                pivot= list[0]
-                less = [i for i in list[1:] if i <= pivot]
-                greater = [i for i in list[1:] if i > pivot]
                 return manual_sort(greater, order="desc") + [pivot] + manual_sort(less, order="desc")
-            
-data = [5,2,1,6,4,34,2365,234,236,642,62345,435]
+
+#Selection sort. Also pretty slow
+def selection_manual_sort(list: list, *, order: str) -> int:
+    for i in range(len(list)):
+        minimum_index = i
+        for j in range(i + 1, len(list)):
+            match order:
+                case "asc":
+                    if list[j] < list[minimum_index]:
+                        minimum_index = j
+                case "desc":
+                    if list[j] > list[minimum_index]:
+                        minimum_index = j
+        list[i], list[minimum_index] = list[minimum_index], list[i]
+    return list    
+
+data = [4,3,2,6,5]
+
 
 if __name__ == "__main__":
     print(manual_sort(data, order="asc"))
